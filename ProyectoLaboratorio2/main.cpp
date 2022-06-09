5601d25d4fc76aa9b910ca065167b1666e15f240
Luka <94508681+Lukiitaaa@users.noreply.github.com>
2022-06-09 13:32:16 -0300

    Agregue la funcion rlutil, intercambie el cin de opcion por una entrada mas directa con F1,F2,F3 etc... y agregue el recuadro en el menu principal despues agrego los que faltan

--- ProyectoLaboratorio2/main.cpp
+++ ProyectoLaboratorio2/main.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 using namespace std;
+#include "funcionesrecuadro.h"
 
 ///funciones adiccionales para los demas menus
 int Alumnos();
@@ -14,38 +15,78 @@ int main()
     while(true)
     {
         system("cls");
+        recuadro(1,1,60,20);
+        recuadro(8,4,46,15);
+        rlutil::locate(23,3);
         cout<< "\t MENU PRINCIPAL "<<endl;
-        cout<< "-------------------------------"<<endl;
-        cout<< "1) INGRESAR MENU ALUMNO"<<endl;
-        cout<< "2) INGRESAR MENU MAESTRO"<<endl;
-        cout<< "3) INGRESAR MENU NOTAS Y EXAMEN"<<endl;
-        cout<< "4) INGRESAR MENU GRADOS"<<endl;
-        cout<< "5) INGRESAR MENU MATERIA"<<endl;
-        cout<< "0) Salir"<<endl;
-        cout<< "-------------------------------"<<endl;
-        cout<< "Opcion: ";
-        cin>>Opc;
-        system("cls");
+        rlutil::locate(11,7);
+        cout<< "F1 - INGRESAR MENU ALUMNO"<<endl;
+        rlutil::locate(11,9);
+        cout<< "F2 - INGRESAR MENU MAESTRO"<<endl;
+        rlutil::locate(11,11);
+        cout<< "F3 - INGRESAR MENU NOTAS Y EXAMEN"<<endl;
+        rlutil::locate(11,13);
+        cout<< "F4 - INGRESAR MENU GRADOS"<<endl;
+        rlutil::locate(11,15);
+        cout<< "F5 - INGRESAR MENU MATERIA"<<endl;
+        rlutil::locate(11,17);
+        cout<< "F6 - Salir"<<endl;
+        rlutil::locate(0,0);
+       // cout<< "Opcion: ";
+       // cin>>Opc;
+       // system("cls");
+        Opc=rlutil::getkey();
         switch(Opc)
         {
-        case 1:
+        case 18:
             Alumnos();
             break;
-        case 2:
+        case 19:
             Maestros();
             break;
-        case 3:
+        case 20:
             ExamenNotas();
             break;
-        case 4:
+        case 21:
             Grados();
             break;
-        case 5:
+        case 22:
             Materias();
             break;
-        case 0:
+        case 23:
+          salida();
+          char salir;
+          rlutil::locate(23,6);
+          cin >> salir;
+          if(salir=='s'|| salir=='S'){
+            rlutil::locate(16,10);
+            cout << "Gracias por utilizar el programa!" << endl;
+            rlutil::locate(1,22);
             return 0;
+
+            }
+          else if(salir=='n'|| salir=='N'){
+            rlutil::locate(10,20);
+            system("pause");
+            system("cls");
+
+          }
+
+          break;
+
+        default:
+
+            recuadro(1,1,60,20);
+            rlutil::locate(17,10);
+            rlutil::setColor(rlutil::BLACK);
+            cout << "Comando incorrecto!" << endl;
+            rlutil::setColor(rlutil::BLACK);
+            rlutil::locate(10,20);
+            system("pause");
+            system("cls");
             break;
+
+
 
 
         }
