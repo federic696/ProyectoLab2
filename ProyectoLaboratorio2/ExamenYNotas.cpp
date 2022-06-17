05a0634855a270226a44d69600507c38ca5f1279
PeterzzCode <pedroadominguez@hotmail.com>
2022-06-17 00:49:09 -0300

    Hice los Puntos 1,2 y 3 De la Clase ExamenYNotas

--- ProyectoLaboratorio2/ExamenYNotas.cpp
+++ ProyectoLaboratorio2/ExamenYNotas.cpp
@@ -1,19 +1,23 @@
 #include<iostream>
 #include<conio.h>
 using namespace std;
+#include "ExamenYNotas.h"
 
 int ExamenNotas(){
     int Opc;
+    ExamenYNotas obj;
+    int pos=0;
     while(true)
     {
         system("cls");
         cout<< "\t MENU Examen y Notas "<<endl;
         cout<< "-------------------------------"<<endl;
         cout<< "1) Cargar nuevo examen"<<endl;
-        cout<< "2) Modificar examen"<<endl;
-        cout<< "3) Modificar nota alumnos"<<endl;
+        cout<< "2) Modificar ID Alumno Por Examen"<<endl;
+        cout<< "3) Modificar Nota Por Examen Del Alumno"<<endl;
         cout<< "4) Cargar promedio alumno por grado"<<endl;
         cout<< "5) Ver mejores notas de alumnos"<<endl;
+        cout<< "9) Mostrar Examenes (Provisorio)"<<endl;
         cout<< "0) Volver a menu"<<endl;
         cout<< "-------------------------------"<<endl;
         cout<< "Opcion: "<<endl;
@@ -22,16 +26,23 @@ int ExamenNotas(){
         switch(Opc)
         {
         case 1:
-
+            obj.cargarExamenYNotas();
+            obj.GrabarEnDiscoExamenYNotas();
             break;
         case 2:
-
+            cambiarIDAlumno();
             break;
         case 3:
-
+            cambiarNota();
             break;
         case 4:
-
+            break;
+        case 9:
+            while(obj.LeerEnDiscoExamenYNotas(pos)){
+                obj.mostrarExamenYNotas();
+                pos++;
+            }
+            system("pause");
             break;
         case 0:
             return 0;
