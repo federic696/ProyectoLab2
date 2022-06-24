44761142a49dc926189280a1fa30724b2c42a6ca
Luka <94508681+Lukiitaaa@users.noreply.github.com>
2022-06-24 19:09:04 -0300

    Solucione examen y notas front

--- ProyectoLaboratorio2/ExamenYNotas.cpp
+++ ProyectoLaboratorio2/ExamenYNotas.cpp
@@ -102,6 +102,8 @@ if(pos2>=0){
 return false;
 }
 
+
+
 void cambiarNota(){
 int Nota;
 int pos;
@@ -124,6 +126,119 @@ cin >> Nota;
 if(ModificarNotaEnDisco(pos, Nota)){cout << "SE MODIFICO LA NOTA"<<endl;}
 
 }
+
+void recuadroexa1(int x, int y, int ancho, int alto){
+
+const char *UI_BOTTOM_RIGHT = "\xD9"; // 217 - ┘
+const char *UI_BOTTOM_LEFT = "\xC0"; // 192 - └
+const char *UI_TOP_LEFT = "\xDA"; // 218 - ┌
+const char *UI_TOP_RIGHT = "\xBF"; // 191 - ┐
+
+const char *UI_HORIZONTAL_LINE = "\xC4"; // 196 - ─
+
+
+const char *UI_VERTICAL_LINE = "\xB3"; // 179 - │
+
+
+
+
+    /// Borramos el espacio del recuadro
+    int i, j;
+    for(i=x; i<=x+ancho; i++){
+        for(j=y; j<=y+alto; j++){
+            rlutil::setBackgroundColor(rlutil::DARKGREY);
+            rlutil::locate(i, j);
+            cout << " ";
+
+        }
+    }
+    rlutil::setColor(rlutil::YELLOW);
+    /// Líneas horizontales
+    for(i=x; i<=x+ancho; i++){
+        rlutil::locate(i, y);
+        cout << UI_HORIZONTAL_LINE;
+        rlutil::locate(i,4);
+        cout << UI_HORIZONTAL_LINE;
+        rlutil::locate(i, y+alto);
+        cout << UI_HORIZONTAL_LINE;
+
+    }
+    /// Líneas verticales
+    for(i=y; i<=y+alto; i++){
+        rlutil::locate(x, i);
+        cout << UI_VERTICAL_LINE;
+        rlutil::locate(x+ancho, i);
+        cout << UI_VERTICAL_LINE;
+    }
+    /// Vértices
+    rlutil::locate(x, y);
+    cout << UI_TOP_LEFT;
+    rlutil::locate(x, y+alto);
+    cout << UI_BOTTOM_LEFT;
+    rlutil::locate(x+ancho, y);
+    cout << UI_TOP_RIGHT;
+    rlutil::locate(x+ancho, y+alto);
+    cout << UI_BOTTOM_RIGHT;
+}
+
+void recuadroexa(int x, int y, int ancho, int alto){
+
+const char *UI_BOTTOM_RIGHT = "\xD9"; // 217 - ┘
+const char *UI_BOTTOM_LEFT = "\xC0"; // 192 - └
+const char *UI_TOP_LEFT = "\xDA"; // 218 - ┌
+const char *UI_TOP_RIGHT = "\xBF"; // 191 - ┐
+
+const char *UI_HORIZONTAL_LINE = "\xC4"; // 196 - ─
+
+
+const char *UI_VERTICAL_LINE = "\xB3"; // 179 - │
+
+
+
+
+    /// Borramos el espacio del recuadro
+    //COLOR DE FONDO
+    int i, j;
+    for(i=x; i<=x+ancho; i++){
+        for(j=y; j<=y+alto; j++){
+            rlutil::setBackgroundColor(rlutil::DARKGREY);
+            rlutil::locate(i, j);
+            cout << " ";
+
+        }
+    }
+    //COLOR DE LINEAS Y LETRAS
+    rlutil::setColor(rlutil::YELLOW);
+    /// Líneas horizontales
+    for(i=x; i<=x+ancho; i++){
+        rlutil::locate(i, y);
+        cout << UI_HORIZONTAL_LINE;
+        rlutil::locate(i, y+alto);
+        cout << UI_HORIZONTAL_LINE;
+
+    }
+    /// Líneas verticales
+    for(i=y; i<=y+alto; i++){
+        rlutil::locate(x, i);
+        cout << UI_VERTICAL_LINE;
+        rlutil::locate(x+ancho, i);
+        cout << UI_VERTICAL_LINE;
+    }
+    //COLOR DE LETRAS
+    /// Vértices
+    rlutil::locate(x, y);
+    cout << UI_TOP_LEFT;
+    rlutil::locate(x, y+alto);
+    cout << UI_BOTTOM_LEFT;
+    rlutil::locate(x+ancho, y);
+    cout << UI_TOP_RIGHT;
+    rlutil::locate(x+ancho, y+alto);
+    cout << UI_BOTTOM_RIGHT;
+    rlutil::resetColor();
+    rlutil::setBackgroundColor(rlutil::DARKGREY);
+}
+
+
 
 void ExamenYNotas::cargarExamenYNotas()
 {
