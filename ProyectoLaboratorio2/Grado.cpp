40986645283d7f07ef59879b3de491ec2318f477
Luka <94508681+Lukiitaaa@users.noreply.github.com>
2022-06-25 17:18:50 -0300

    Front grado

--- ProyectoLaboratorio2/Grado.cpp
+++ ProyectoLaboratorio2/Grado.cpp
@@ -1,8 +1,120 @@
 #include<iostream>
 #include<conio.h>
 #include "Grado.h"
+#include "rlutil.h"
 
 using namespace std;
+
+void recuadrogra1(int x, int y, int ancho, int alto){
+
+const char *UI_BOTTOM_RIGHT = "\xD9"; // 217 - ┘
+const char *UI_BOTTOM_LEFT = "\xC0"; // 192 - └
+const char *UI_TOP_LEFT = "\xDA"; // 218 - ┌
+const char *UI_TOP_RIGHT = "\xBF"; // 191 - ┐
+
+const char *UI_HORIZONTAL_LINE = "\xC4"; // 196 - ─
+
+
+const char *UI_VERTICAL_LINE = "\xB3"; // 179 - │
+
+
+
+
+    /// Borramos el espacio del recuadro
+    int i, j;
+    for(i=x; i<=x+ancho; i++){
+        for(j=y; j<=y+alto; j++){
+            rlutil::setBackgroundColor(rlutil::DARKGREY);
+            rlutil::locate(i, j);
+            cout << " ";
+
+        }
+    }
+    rlutil::setColor(rlutil::YELLOW);
+    /// Líneas horizontales
+    for(i=x; i<=x+ancho; i++){
+        rlutil::locate(i, y);
+        cout << UI_HORIZONTAL_LINE;
+        rlutil::locate(i,4);
+        cout << UI_HORIZONTAL_LINE;
+        rlutil::locate(i, y+alto);
+        cout << UI_HORIZONTAL_LINE;
+
+    }
+    /// Líneas verticales
+    for(i=y; i<=y+alto; i++){
+        rlutil::locate(x, i);
+        cout << UI_VERTICAL_LINE;
+        rlutil::locate(x+ancho, i);
+        cout << UI_VERTICAL_LINE;
+    }
+    /// Vértices
+    rlutil::locate(x, y);
+    cout << UI_TOP_LEFT;
+    rlutil::locate(x, y+alto);
+    cout << UI_BOTTOM_LEFT;
+    rlutil::locate(x+ancho, y);
+    cout << UI_TOP_RIGHT;
+    rlutil::locate(x+ancho, y+alto);
+    cout << UI_BOTTOM_RIGHT;
+}
+void recuadrogra(int x, int y, int ancho, int alto){
+
+const char *UI_BOTTOM_RIGHT = "\xD9"; // 217 - ┘
+const char *UI_BOTTOM_LEFT = "\xC0"; // 192 - └
+const char *UI_TOP_LEFT = "\xDA"; // 218 - ┌
+const char *UI_TOP_RIGHT = "\xBF"; // 191 - ┐
+
+const char *UI_HORIZONTAL_LINE = "\xC4"; // 196 - ─
+
+
+const char *UI_VERTICAL_LINE = "\xB3"; // 179 - │
+
+
+
+
+    /// Borramos el espacio del recuadro
+    //COLOR DE FONDO
+    int i, j;
+    for(i=x; i<=x+ancho; i++){
+        for(j=y; j<=y+alto; j++){
+            rlutil::setBackgroundColor(rlutil::DARKGREY);
+            rlutil::locate(i, j);
+            cout << " ";
+
+        }
+    }
+    //COLOR DE LINEAS Y LETRAS
+    rlutil::setColor(rlutil::YELLOW);
+    /// Líneas horizontales
+    for(i=x; i<=x+ancho; i++){
+        rlutil::locate(i, y);
+        cout << UI_HORIZONTAL_LINE;
+        rlutil::locate(i, y+alto);
+        cout << UI_HORIZONTAL_LINE;
+
+    }
+    /// Líneas verticales
+    for(i=y; i<=y+alto; i++){
+        rlutil::locate(x, i);
+        cout << UI_VERTICAL_LINE;
+        rlutil::locate(x+ancho, i);
+        cout << UI_VERTICAL_LINE;
+    }
+    //COLOR DE LETRAS
+    /// Vértices
+    rlutil::locate(x, y);
+    cout << UI_TOP_LEFT;
+    rlutil::locate(x, y+alto);
+    cout << UI_BOTTOM_LEFT;
+    rlutil::locate(x+ancho, y);
+    cout << UI_TOP_RIGHT;
+    rlutil::locate(x+ancho, y+alto);
+    cout << UI_BOTTOM_RIGHT;
+    rlutil::resetColor();
+    rlutil::setBackgroundColor(rlutil::DARKGREY);
+}
+
 
 void Grado::cargarGrado()
 {
@@ -43,32 +155,37 @@ int Opc;
     while(true)
     {
         system("cls");
-        cout<< "\t MENU GRADO "<<endl;
-        cout<< "-------------------------------"<<endl;
-        cout<< "1) Mostrar  maestros por grado y materia"<<endl;
-        cout<< "2) Mostrar  estudiante por grado"<<endl;
-        cout<< "3) Mostrar mejores promedios por grado"<<endl;
-        cout<< "4) Mostrar mejores promedio materia por grado"<<endl;
-        cout<< "0) Volver a menu"<<endl;
-        cout<< "-------------------------------"<<endl;
-        cout<< "Opcion: "<<endl;
-        cin>>Opc;
-        system("cls");
+        recuadrogra(1,1,60,20);
+        recuadrogra1(1,1,60,20);
+        rlutil::locate(23,3);
+        cout<< "| MENU GRADOS |"<<endl;
+        rlutil::locate(11,7);
+        cout<< "F1 - Mostrar  maestros por grado y materia"<<endl;
+        rlutil::locate(11,9);
+        cout<< "F2 - Mostrar  estudiante por grado"<<endl;
+        rlutil::locate(11,10);
+        cout<< "F3 - Mostrar mejores promedios por grado"<<endl;
+        rlutil::locate(11,12);
+        cout<< "F4 - Mostrar mejores promedio materia por grado"<<endl;
+        rlutil::locate(11,13);
+        cout<< "F5 - Volver a menu"<<endl;
+        rlutil::locate(0,0);
+        Opc=rlutil::getkey();
         switch(Opc)
         {
-        case 1:
+        case 18:
 
             break;
-        case 2:
+        case 19:
 
             break;
-        case 3:
+        case 20:
 
             break;
-        case 4:
+        case 21:
 
             break;
-        case 0:
+        case 22:
             return 0;
             break;
 
