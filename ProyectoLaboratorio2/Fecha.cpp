0426b16e55354a10e4c8d11e776980e91842cd65
Maxilago <maxilago_98@hotmail.com>
2022-07-14 02:43:13 -0300

    Chequeo de fecha valida

--- ProyectoLaboratorio2/Fecha.cpp
+++ ProyectoLaboratorio2/Fecha.cpp
@@ -14,10 +14,18 @@ void Fecha::CargarFecha()
     rlutil::locate(4,14);
     cout<<"DIA: ";
     cin>>d;
+    while(d<1 || d>31){
+        cout<<"ELIGA UN DIA VALIDO: ";
+        cin>>d;
+    }
     setDia(d);
     rlutil::locate(4,15);
     cout<<"MES: ";
     cin>>d;
+    while(d<1 || d>12){
+        cout<<"ELIGA UN MES VALIDO: ";
+        cin>>d;
+    }
     setMes(d);
     rlutil::locate(4,16);
     cout<<"ANIO: ";
